d9ab67bdff11f65f139198da78720046e897b8c7
Micael Jerry <hei.jerry.2@gmail.com>
2023-05-07 20:30:52 +0300

    style: register page

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -20,45 +20,27 @@ const Register = () => {
   return (
     <div>
       <div>
-        <div>
+        <div className="container">
           <form onSubmit={submitHandler}>
-            <h1>Register</h1>
-            <div>
-              <label htmlFor="name_field">Name</label>
-              <input
-                type="text"
-                id="name_field"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+            <div className="mb-3">
+              <h1>Register</h1>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="name_field" className="form-label">Name</label>
+              <input type="text" className="form-control" id="name_field" value={name} onChange={(e) => setName(e.target.value)}
               />
             </div>
-            <div >
-              <label htmlFor="email_field">
-                Email address
-              </label>
-              <input
-                type="email"
-                id="email_field"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
+            <div className="mb-3">
+              <label htmlFor="email_field" className="form-label">Email address</label>
+              <input className="form-control" type="email" id="email_field" value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
-            <div>
-              <label htmlFor="password_field">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password_field"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
+            <div className="mb-3">
+              <label htmlFor="password_field" className="form-label">Password</label>
+              <input type="password" className="form-control" id="password_field" value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
-            <button
-              type="submit"
-            >
-              Register
-            </button>
+            <div className="mb-3">
+              <button type="submit" className="btn btn-primary">Register</button>
+            </div>
           </form>
         </div>
       </div>
