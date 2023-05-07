29a2694d45c122422702b607da39c3de9d89807e
Micael Jerry <hei.jerry.2@gmail.com>
2023-05-07 21:06:08 +0300

    chore: update nextauth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { getUserByEmailToLocalStorage  } from "@/storage/localStorage";
+import { getUserByEmailToLocalStorage } from "@/storage/localStorage";
 
 export default NextAuth({
   session: {
@@ -10,10 +10,7 @@ export default NextAuth({
     CredentialsProvider({
       async authorize(credentials, req) {
         const { email, password } = credentials;
-
-        console.log(email, password);
-        
-        const user = await getUserByEmailToLocalStorage(email);
+        const user = getUserByEmailToLocalStorage(email);
         if (!user) {
           throw new Error("Invalid Email or Password");
         }
