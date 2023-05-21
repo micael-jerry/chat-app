ab00fed2bb920edff3228ae906243b27bd4bf61e
Micael Jerry <hei.jerry.2@gmail.com>
2023-05-21 16:43:24 +0300

    fix: protect route

--- src/pages/global/index.tsx
+++ src/pages/global/index.tsx
@@ -1,5 +1,21 @@
 import { NavBar } from "@/components/NavBar";
+import { getSession } from "next-auth/react";
 import React from "react";
+
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context)
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+  return {
+    props: { session }
+  }
+}
 
 const Global = () => {
   return (
