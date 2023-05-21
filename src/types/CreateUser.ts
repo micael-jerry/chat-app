d40b5cd646099fde16468efc6c2ee423cd3069cd
Micael Jerry <hei.jerry.2@gmail.com>
2023-05-21 12:32:32 +0300

    fix: get session server side to /profile

--- src/types/CreateUser.ts
+++
@@ -1,6 +0,0 @@
-export type CreateUser = {
-  name: string;
-  email: string;
-  bio: string | null;
-  password: string;
-};
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,15 +1,22 @@
-export type User = {
-  id: number;
-  email: string;
+export type CreateUser = {
   name: string;
-  googleId: string | null;
+  email: string;
   bio: string | null;
-  status: number;
-  createdAt: string | null;
-  updatedAt: string | null;
-  deletedAt: string | null;
-  token: string;
+  password: string;
 };
+
+export type User = {
+  id?: number;
+  email?: string | null;
+  name?: string | null;
+  googleId?: string | null;
+  bio?: string | null;
+  status?: number;
+  createdAt?: string | null;
+  updatedAt?: string | null;
+  deletedAt?: string | null;
+  token?: string;
+} | undefined
 
 export type LogResponse = {
   status: boolean;
